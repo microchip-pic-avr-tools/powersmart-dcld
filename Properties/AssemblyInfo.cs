7e6880db9855d64a3eca0e101208a2cb4339f3a7
M91406 <andreas.reiter@microchip.com>
2019-07-12 10:16:07 +0200

    Updated assembly version and created a new install picture

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.59")]
-[assembly: AssemblyFileVersion("0.9.0.59")]
+[assembly: AssemblyVersion("0.9.0.61")]
+[assembly: AssemblyFileVersion("0.9.0.61")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
