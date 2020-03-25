09ecb21dd5e74adbd7e8ba5d1e9aae77e136bf28
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-03-25 23:43:21 +0100

    Added class Globals to establish classic, global variables which can be accessed from all modules

---
+++ clsGlobals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dcld
+{
+    public static class dcldGlobals
+    {
+        // Examples for Global Variables:
+
+        // public const Int32 BUFFER_SIZE = 512; // Unmodifiable
+        // public static String FILE_NAME = "Output.txt"; // Modifiable
+        // public static readonly String CODE_PREFIX = "US-"; // Unmodifiable
+
+        // The application version key is an encoded version number represented as Integer
+        public static int APP_VERSION_KEY = 0; 
+
+    }
+}
