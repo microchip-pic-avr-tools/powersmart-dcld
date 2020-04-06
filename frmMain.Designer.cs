b864155f686b6497878527cb376d2a3aff4ca4aa
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-04-06 16:35:57 +0200

    Designer clutter

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -1909,10 +1909,10 @@
             this.tabSourceCodeConfig.Controls.Add(this.grpCodeFeatureOptions);
             this.tabSourceCodeConfig.Controls.Add(this.grpContextManagement);
             this.tabSourceCodeConfig.Controls.Add(this.grpFunctionLabel);
-            this.tabSourceCodeConfig.Location = new System.Drawing.Point(4, 24);
+            this.tabSourceCodeConfig.Location = new System.Drawing.Point(4, 22);
             this.tabSourceCodeConfig.Name = "tabSourceCodeConfig";
             this.tabSourceCodeConfig.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSourceCodeConfig.Size = new System.Drawing.Size(428, 743);
+            this.tabSourceCodeConfig.Size = new System.Drawing.Size(428, 745);
             this.tabSourceCodeConfig.TabIndex = 1;
             this.tabSourceCodeConfig.Text = "Source Code Configuration";
             this.tabSourceCodeConfig.UseVisualStyleBackColor = true;
