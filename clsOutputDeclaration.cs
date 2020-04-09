ee419d4ff5cfb07e2e0563d335b5502fc3cfa51d
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-04-10 01:34:17 +0200

    Output gain and output value calculations have been separated into individual calculations for each converter type. Now users can specify Nominal operation points, which are used to provide nominal output values. .This was required to support P-Term controller coefficient calculation.

--- clsOutputDeclaration.cs
+++ clsOutputDeclaration.cs
@@ -13,6 +13,20 @@ namespace dcld
 
         // Feedback Type Declaration ~~~~~~~~~~~~~~~~~~~
 
+        internal enum dcldConverterType : byte
+        {
+            DCLD_CONVERTER_BUCK = 0,
+            DCLD_CONVERTER_BOOST = 1,
+            DCLD_CONVERTER_BUCK_BOOST = 2
+        }
+
+        private dcldConverterType _ConverterType = dcldConverterType.DCLD_CONVERTER_BUCK;
+        internal dcldConverterType ConverterType
+        {
+            get { return(_ConverterType); }
+            set { _ConverterType = value; return; }
+        }
+
         internal enum dcldOutputType : byte
         {
             DCLD_OUT_TYPE_UNDEFINED = 0,
@@ -29,7 +43,58 @@ namespace dcld
             set { _OutputType = value; return; }
         }
 
-        // Feedback Type Declaration End ~~~~~~~~~~~~~~~
+        // Output Type Declaration End ~~~~~~~~~~~~~~~
+
+        internal enum dcldDeviceType : byte
+        {
+            DCLD_DEVICE_TYPE_UNDEFINED = 0,
+            DCLD_DEVICE_TYPE_P33FJ = 1,
+            DCLD_DEVICE_TYPE_P33EP = 2,
+            DCLD_DEVICE_TYPE_P33C = 3
+        }
+
+        private dcldDeviceType _DeviceType = dcldDeviceType.DCLD_DEVICE_TYPE_P33C;
+        internal dcldDeviceType DeviceType
+        {
+            get { return (_DeviceType); }
+            set { _DeviceType = value; refreshPWM(); return; }
+        }
+
+        internal bool SetDeviceType (string DeviceTypeString)
+        {
+            if (DeviceTypeString.Trim().Length == 0)
+                return (false);
+
+            if (DeviceTypeString.Contains("dsPIC33C"))
+                _DeviceType = dcldDeviceType.DCLD_DEVICE_TYPE_P33C;
+            else if (DeviceTypeString.Contains("dsPIC33EP"))
+                _DeviceType = dcldDeviceType.DCLD_DEVICE_TYPE_P33EP;
+            else if (DeviceTypeString.Contains("dsPIC33FJ"))
+                _DeviceType = dcldDeviceType.DCLD_DEVICE_TYPE_P33FJ;
+            else
+                return (false);
+
+            return (true);
+        }
+        
+        internal string DeviceTypeString
+        {
+            get {
+
+                switch (_DeviceType)
+                {
+                    case dcldDeviceType.DCLD_DEVICE_TYPE_P33C:
+                        return ("dsPIC33C");
+                    case dcldDeviceType.DCLD_DEVICE_TYPE_P33EP:
+                        return ("dsPIC33EP");
+                    case dcldDeviceType.DCLD_DEVICE_TYPE_P33FJ:
+                        return ("dsPIC33FJ");
+                    default:
+                        return("");
+                }
+            }
+        }
+
 
         // PWM Properties ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         private double _PWMclock = (4e+9);
@@ -46,10 +111,12 @@ namespace dcld
             set { _PWMClkDiv = value; refreshPWM(); return; }
         }
 
-        private double _PWMRes = (250e-12);
         internal double PWMResolution
         {
-            get { return (_PWMRes); }
+            get { 
+                if (_PWMClkDiv == 0.0) _PWMClkDiv = 1.0;
+                return (1.0 / (_PWMclock / _PWMClkDiv)); 
+            }
         }
 
         private double _PWMBitRes = 16.0;
@@ -66,16 +133,26 @@ namespace dcld
             set { _PWMfreq = value; refreshPWM(); return; }
         }
 
-        private double _PWMper = (4e-6);
         internal double PWMPeriod
         {
-            get { return (_PWMper); }
+            get { return (1.0 / _PWMfreq); }
         }
 
-        private Int64 _PWMcount = 8000;
-        internal Int64 PWMCount
+        internal Int64 PWMPeriodCount
         {
-            get { return (_PWMcount); }
+            get { return (Convert.ToInt64(Math.Round((PWMPeriod / PWMResolution), 0))); }
+        }
+
+        private double _PWMduty = (0.30);
+        internal double PWMDutyCycle
+        {
+            get { return (_PWMduty); }
+            set { _PWMduty = value; refreshPWM(); return; }
+        }
+
+        internal Int64 PWMDutyCycleCount
+        {
+            get { return (Convert.ToInt64((double)_PWMduty * PWMPeriodCount)); }
         }
 
         private double _Gain = 1.000;
@@ -84,38 +161,108 @@ namespace dcld
             get { return (_Gain); }
         }
 
+        private double _NominalOutput = 0.0;
+        internal double NominalOutput
+        {
+            get { return (_NominalOutput); }
+        }
+
+        private double _NominalInputVoltage = 0.0;
+        internal double NominalInputVoltage
+        {
+            get { return (_NominalInputVoltage); }
+            set { _NominalInputVoltage = value; refreshDutyCycle(); return; }
+        }
+
+        private double _NominalOutputVoltage = 0.0;
+        internal double NominalOutputVoltage
+        {
+            get { return (_NominalOutputVoltage); }
+            set { _NominalOutputVoltage = value; refreshDutyCycle(); return; }
+        }
+
+        private double _WindingRatioPrimary = 1.0;
+        internal double WindingRatioPrimary
+        {
+            get { return (_WindingRatioPrimary); }
+            set {
+                double _dbl_dum = 0.0;
+                _dbl_dum = value;
+                if (_dbl_dum == 0.0) _dbl_dum = 1;
+                _WindingRatioPrimary = _dbl_dum; 
+                refreshDutyCycle(); 
+                return;
+            }
+        }
+
+        private double _WindingRatioSecondary = 1.0;
+        internal double WindingRatioSecondary
+        {
+            get { return (_WindingRatioSecondary); }
+            set {
+                double _dbl_dum = 0.0;
+                _dbl_dum = value;
+                if (_dbl_dum == 0.0) _dbl_dum = 1;
+                _WindingRatioSecondary = _dbl_dum; 
+                refreshDutyCycle(); 
+                return;
+            }
+        }
+
+        internal double WindingRatio
+        {
+            get { return (_WindingRatioPrimary / _WindingRatioSecondary); }
+        }
+
+        private void refreshDutyCycle()
+        {
+            double _voutX = 0.0, _wr = 0.0;
+
+            _wr = (_WindingRatioPrimary / _WindingRatioSecondary);
+
+            switch (_ConverterType)
+            {
+                case clsOutputDeclaration.dcldConverterType.DCLD_CONVERTER_BUCK: // Buck/Forward type
+                    _voutX = _NominalOutputVoltage / _wr;
+                    PWMDutyCycle = _voutX / _NominalInputVoltage;
+                    break;
+                case clsOutputDeclaration.dcldConverterType.DCLD_CONVERTER_BOOST: // Boost type
+                    _voutX = _NominalOutputVoltage / _wr;
+                    PWMDutyCycle = ((_voutX - _NominalInputVoltage) / _voutX); 
+                    break;
+                case clsOutputDeclaration.dcldConverterType.DCLD_CONVERTER_BUCK_BOOST: // Buck/Boost type
+                    _voutX = _NominalOutputVoltage / _wr;
+                    PWMDutyCycle = (_voutX) / (_voutX + _NominalInputVoltage);
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
         private void refreshPWM()
         {
-
-            if (_PWMClkDiv == 0.0) _PWMClkDiv = 1;
-            _PWMRes = 1.0 / (_PWMclock / _PWMClkDiv);
 
             switch(_OutputType)
             {
                 case dcldOutputType.DCLD_OUT_TYPE_FIXED_FREQUENCY:
-                    _PWMper = 1.0 / _PWMfreq; 
-                    _PWMcount = Convert.ToInt64(Math.Floor(_PWMper / _PWMRes) + 1);
+                    _NominalOutput = (PWMDutyCycle * PWMPeriodCount);
                     break;
 
                 case dcldOutputType.DCLD_OUT_TYPE_PHASE_SHIFTED_PWM:
-                    _PWMper = 1.0 / _PWMfreq; 
-                    _PWMcount = Convert.ToInt64((Math.Floor(_PWMper / _PWMRes) + 1) / 2.0);
+                    _NominalOutput = (PWMPeriodCount / 2);
                     break;
 
                 case dcldOutputType.DCLD_OUT_TYPE_VARIABLE_FREQUENCY:
-                    _PWMcount = Convert.ToInt64(Math.Pow(2.0, _PWMBitRes)-1);
-                    _PWMper = _PWMRes * _PWMcount;
-                    _PWMfreq = 1.0 / _PWMper;
+                    _NominalOutput = PWMPeriodCount;
                     break;
 
                 default:
-                    _PWMcount = Convert.ToInt64(Math.Pow(2.0, _PWMBitRes) - 1);
-                    _PWMper = _PWMRes * _PWMcount;
-                    _PWMfreq = 1.0 / _PWMper;
                     break;
             }
 
-            _Gain = _PWMcount / (Math.Pow(2.0, _PWMBitRes)-1);
+            _Gain = PWMPeriodCount / (Math.Pow(2.0, _PWMBitRes)-1);
 
         }
 
